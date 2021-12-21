44d6a1bd654105b323e69a2969cf9bcec85cca4e
Alejandro M. Coca <alxmcr@outlook.com>
2021-12-21 15:31:04 -0400

    Create the AppLogo component

---
+++ src/components/common/AppLogo/index.tsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import logo from "../../../assets/svg/icon-rocket-brand.svg";
+import "./AppLogo.scss";
+
+export const AppLogo = () => {
+  return (
+    <div className="app-logo">
+      <Link to="/" className="app-logo__link">
+        <img src={logo} alt="logo" className="app-logo__image" loading="lazy" />
+      </Link>
+      <Link to="/" className="app-logo__link app-logo__link--name">
+        Spacifly
+      </Link>
+    </div>
+  );
+};
